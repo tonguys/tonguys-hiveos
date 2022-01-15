906b0f624df9be49940a1e20f448fc95d10354a6
Roma004 <klimov.roma04@yandex.ru>
2022-01-15 17:36:17 +0300

    hiveos_test_2

--- tonguys_pool/h-config.sh
+++ tonguys_pool/h-config.sh
@@ -1,13 +1,9 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 
 SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 
-if [[ ! $CUSTIOM_URL ]]; then
-    echo "${RED} pool url is NULL! ${NCOLOR}"
-fi
-
-ARGS_LINE="-u $CUSTOM_URL -m $SCRIPT_DIR/tonguys_miner"
+ARGS_LINE="-u server1.tonguys.com -m $SCRIPT_DIR/tonguys_miner"
 
 for i in $(echo $CUSTOM_USER_CONFIG); do
     key=$(echo $i | awk -F '=' '{print $1}') 
@@ -16,8 +12,8 @@ for i in $(echo $CUSTOM_USER_CONFIG); do
     if [[ $key = 'token' ]]; then
         ARGS_LINE="$ARGS_LINE -t $val"
     elif [[ $key = 'gpus' ]]; then
-        ARGS_LINE="$ARGS_LINE -G $val"
+        ARGS_LINE="-G $val $ARGS_LINE"
     fi
 done
 
-echo "CLI_ARGS=$ARGS_LINE" > $CUSTOM_CONFIG_FILENAME
+echo "CLI_ARGS=\"$ARGS_LINE\"" > "$CUSTOM_CONFIG_FILENAME"
