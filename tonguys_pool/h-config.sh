ac9c91524f6a039760c160cecf614c70752c1663
Roma004 <klimov.roma04@yandex.ru>
2022-01-22 12:10:41 +0300

    release v0.1.0

--- tonguys_pool/h-config.sh
+++ tonguys_pool/h-config.sh
@@ -12,7 +12,9 @@ wget $CLIENT_RELEASE_URL -O $SCRIPT_DIR/client.tar.gz
 echo "Extracting client archive..."
 tar -xzf $SCRIPT_DIR/client.tar.gz -C $SCRIPT_DIR
 
-ARGS_LINE="-u server1.tonguys.com -m $SCRIPT_DIR/tonguys_miner"
+ARGS_LINE="-u pool.tonguys.com -m $SCRIPT_DIR/tonguys_miner -L $SCRIPT_DIR/asd.asd"
+
+inds="[]"
 
 for i in $(echo $CUSTOM_USER_CONFIG); do
     key=$(echo $i | awk -F '=' '{print $1}') 
@@ -22,8 +24,19 @@ for i in $(echo $CUSTOM_USER_CONFIG); do
         ARGS_LINE="$ARGS_LINE -t $val"
     elif [[ $key = 'gpus' ]]; then
         ARGS_LINE="-G $val $ARGS_LINE"
+	for gpu in $(echo "${val:1:-1}" | sed "s/,/\n/g"); do
+                if [[ $gpu == *"-"* ]]; then
+                        for (( gpu_i=${gpu: 0:1}; gpu_i<=${gpu:2:2}; gpu_i++ )); do
+                                inds=$(echo "$inds" | jq ".[.| length] |= .+ $gpu_i")
+                        done
+                else
+                        inds=$(echo "$inds" | jq ".[.| length] |= .+ $gpu")
+                fi
+        done
     fi
 done
 
 echo "EXEC_LINE=\"$SCRIPT_DIR/client $ARGS_LINE\"" > "$CUSTOM_CONFIG_FILENAME"
-echo EXEC_LINE=\"$SCRIPT_DIR/client $ARGS_LINE\"
+echo "EXEC_LINE=\"$SCRIPT_DIR/cliechent $ARGS_LINE\""
+echo "GPUS=$(echo $inds | jq -c ".")" >> "$CUSTOM_CONFIG_FILENAME"
+echo "GPUS=$(echo $inds | jq -c ".")"
--- tonguys_pool/h-stats.sh
+++ tonguys_pool/h-stats.sh
@@ -1,40 +1,108 @@
 #!/bin/bash
 
-DIRNAME=$(dirname "$BASH_SOURCE")
-. $DIRNAME/h-manifest.conf
+#-------------------------------------------------------------------------
+# ENVIRONMENT (DETERMINED BY SCRIPT)
+#-------------------------------------------------------------------------
 
-STATS_FILE=/hive/miners/custom/${CUSTOM_NAME}/stats.json
+SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+. $SCRIPT_DIR/h-manifest.conf
+. $SCRIPT_DIR/$CUSTOM_NAME.conf
 
-temp=$(jq '.temp' <<< $gpu_stats)
-fan=$(jq '.fan' <<< $gpu_stats)
-[[ $cpu_indexes_array != '[]' ]] &&
-    temp=$(jq -c "del(.$cpu_indexes_array)" <<< $temp) &&
-    fan=$(jq -c "del(.$cpu_indexes_array)" <<< $fan)
+MINER_KEYS=$GPUS
 
-busids=$(jq '.busids' <<< $gpu_stats)
+#echo "$MINER_KEYS"
+# debug
 
-BUS_NUMBERS=()
-for i in "${!busids[@]}"; do
-  BUS_NUMER_HEX=$(echo ${busids[$i]:0:2} | tr "a-z" "A-Z")
-  BUS_NUM=$(echo "obase=10; ibase=16; $BUS_NUMER_HEX" | bc)
-  BUS_NUMBERS+=($BUS_NUM)
+#-------------------------------------------------------------------------
+# READ GPU STATS FROM HIVE OS
+#-------------------------------------------------------------------------
+
+# fill some arrays from gpu-stats
+temps=(`cat $GPU_STATS_JSON | jq -r ".temp[]"`)
+fans=(`cat $GPU_STATS_JSON | jq -r ".fan[]"`)
+powers=(`cat $GPU_STATS_JSON | jq -r ".power[]"`)
+busids=(`cat $GPU_STATS_JSON | jq -r ".busids[]"`)
+brands=(`cat $GPU_STATS_JSON | jq -r ".brand[]"`)
+indexes=()
+
+#echo "asdasdasdas"
+# filter arrays by $TYPE
+cnt=${#busids[@]}
+for (( i=0; i < $cnt; i++)); do
+	if [[ "${brands[$i]}" != "cpu" ]]; then
+        indexes+=($i)
+        continue
+	else # remove arrays data
+		unset temps[$i]
+		unset fans[$i]
+		unset powers[$i]
+		unset busids[$i]
+		unset brands[$i]
+	fi
 done
 
-if [[ ! -f $STATS_FILE ]]; then
-    echo "$STATS_FILE not found"
-else
-    khs=$(jq .khs $STATS_FILE)
-    hs=$(jq .hs $STATS_FILE)
-    uptime=$(jq .uptime $STATS_FILE)
-    bus_numbers=$(echo "${BUS_NUMBERS[@]}" | jq -s '.')
+#-------------------------------------------------------------------------
+# READ MINER STAT
+#-------------------------------------------------------------------------
 
-    stats=$(jq -n \
+STATUS_HS=()
+STATUS_TEMP=()
+STATUS_FAN=()
+STATUS_BUS_NUMBERS=()
+
+for (( i=0; i < ${#indexes[@]}; i++)); do
+    #echo "GPU ID $i ${busids[${indexes[$i]}]}"
+    BUS_NUMER_HEX=$(echo "${busids[${indexes[$i]}]:0:2}" | tr "a-z" "A-Z")
+    BUS_NUMBER=$(echo "obase=10; ibase=16; $BUS_NUMER_HEX" | bc)
+
+    STATUS_BUS_NUMBERS+=($BUS_NUMBER)
+    STATUS_TEMP+=(${temps[${indexes[$i]}]})
+    STATUS_FAN+=(${fans[${indexes[$i]}]})
+done
+
+# calc total hashrate and uptime
+khs=0
+STATUS_UPTIME=0
+KEYS=($(echo $MINER_KEYS | jq -c -r '.[] | @sh' | tr -d \'))
+STATUS_FILE="$SCRIPT_DIR/stats.json"
+for (( i=0; i < ${#KEYS[@]}; i++)); do
+	KEY=${KEYS[$i]}
+    stt=$(jq -c ".stats[] | select(.device==$KEY)" $STATUS_FILE)
+
+    STATUS_PASSED=$(echo $stt | jq -c ".uptime")
+    if [[ $STATUS_UPTIME < $STATUS_PASSED ]]; then
+        STATUS_UPTIME=$STATUS_PASSED
+    fi
+    STATUS_INSTANT_SPEED=$(echo $stt | jq -r ".hashrate")
+    khs=`echo $khs + $STATUS_INSTANT_SPEED | bc`
+    STATUS_HS+=($STATUS_INSTANT_SPEED)
+done
+
+#-------------------------------------------------------------------------
+# COLLECT
+#-------------------------------------------------------------------------
+
+khs=`echo $khs*1000 | bc`
+hs=$(echo "${STATUS_HS[@]}" | jq -s '.')
+temp=$(echo "${STATUS_TEMP[@]}" | jq -s '.')
+fan=$(echo "${STATUS_FAN[@]}" | jq -s '.')
+bus_numbers=$(echo "${STATUS_BUS_NUMBERS[@]}" | jq -s '.')
+
+#echo $hs $temp $fan $bus_numbers
+stats=$(
+  jq -n \
     --argjson hs "$hs" \
-    --arg uptime "$uptime" \
-    --arg ver "$CUSTOM_VERSION" \
     --argjson temp "$temp" \
     --argjson fan "$fan" \
+    --arg uptime "$STATUS_UPTIME" \
+    --arg ver "$CUSTOM_VERSION" \
     --argjson bus_numbers "$bus_numbers" \
-    '{"hs": $hs, "hs_units": "mhs", "algo": "sha256", "uptime": $uptime, "ver": $ver, "temp": $temp, "fan": $fan, "bus_numbers": $bus_numbers}')
-fi
+    '{"hs": $hs, "hs_units": "mhs", "temp": $temp, "fan": $fan, "uptime": $uptime, "ver": $ver, "bus_numbers":$bus_numbers}' <<<"$stats_raw"
+)
 
+[[ -z $khs ]] && khs=0
+[[ -z $stats ]] && stats="null"
+
+# debug
+echo $stats
+echo $khs
